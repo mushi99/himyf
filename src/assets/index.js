8e44e398293bad6c43f0519a6c98f7e2506e6959
kwankurn <heryokakurnnnn@student.telkomuniversity.ac.id>
2025-02-13 14:46:42 +0700

    relationship

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+
+import picture from "./picture.png";
+
+import seasons from "./seasons.jpg";
+import lany from "./lany.png";
+import onbended from "./onbended.jpg";
+import herewithme from "./herewithme.jpg";
+import givemeyour from "./givemeyour.jpg";
+import honey from "./honey.jpg";
+import always from "./always.jpg";
+import mylove from "./mylove.jpg";
+
+
+import fireworks from "./fireworks.gif";
+import mocha from "./mocha.gif";
+
+
+
+
+export {
+
+picture,
+seasons,
+mocha,
+fireworks,
+lany,
+onbended,
+herewithme,
+givemeyour,
+honey,
+always,
+mylove,
+};
